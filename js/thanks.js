c4551617f56209b97d2357816e0fef3f3777e514
unknown <wabrodor@gmail.com>
2021-12-16 00:40:29 +0200

    update-fix

---
+++ js/thanks.js
@@ -0,0 +1,18 @@
+
+
+const counter = document.querySelector(".count")
+let count = 0;
+
+// if(count > 5) return
+
+    setInterval(() => {
+        if(count > 5) return
+        counter.textContent = `${count++}s` 
+     },1000);
+
+
+window.addEventListener("DOMContentLoaded", ()=>{
+    setTimeout(() => {
+        window.location.href = "index.html"
+    }, 5000);
+})
