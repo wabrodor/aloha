f74fb21bd4bec0b3be3b37b9d166c39cbf9f367b
unknown <wabrodor@gmail.com>
2021-12-15 17:24:37 +0200

    update

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const toggle = document.querySelector(".toggle")
 const nav = document.querySelector(".navigation")
 const overlay = document.querySelector(".overlay");
 
+
 const links = document.querySelectorAll("#nav-home");
 
 const navHeight = document.querySelector("nav")
@@ -27,24 +28,25 @@ window.scrollTo(0,0);
 
 window.addEventListener("scroll", ()=>{
     const height = window.scrollY;
-
     const fixedNav = navHeight.getBoundingClientRect().height;
-    
+   
     if(height > fixedNav + 20){
         navHeight.classList.add("fixed")
     }
     else{
         navHeight.classList.remove("fixed")
     }
+    return fixedNav
 })
 
 
 
 links.forEach(link =>{
     link.addEventListener("click", (e)=>{
-        
+        const fixedNav = navHeight.getBoundingClientRect().height;   
     const slicedLink =  link.querySelector("a").getAttribute("href").slice(1);
-
+    const navheight = document.getElementById(slicedLink).getBoundingClientRect().height + fixedNav +600;
+    window.scrollTo(0, 500)
         nav.classList.toggle("active_menu")
     })
 })
