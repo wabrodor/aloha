3343db2df7acec0c0ad3f52e17f8c48a88327665
unknown <wabrodor@gmail.com>
2021-12-16 21:30:43 +0200

    new features added + css updated

--- js/main.js
+++ js/main.js
@@ -1,29 +1,18 @@
 
 
-const navLinks = document.querySelectorAll("#nav-home")
+const navLinks = document.querySelectorAll(".nav-home")
 const toggle = document.querySelector(".toggle")
-const nav = document.querySelector(".navigation")
-const overlay = document.querySelector(".overlay");
-
-
-const links = document.querySelectorAll("#nav-home");
-
+const linksContainer = document.querySelector(".navigation")
+const links = document.querySelectorAll(".nav-home");
 const navHeight = document.querySelector("nav")
 const footer = document.querySelector("footer")
-
 const date = new Date()
 footer.innerHTML = `<P>copyright © ${date.getFullYear()}  ${document.title}  powerd by ALOHA </P>`
 
 
-
 toggle.addEventListener("click", ()=>{
-nav.classList.toggle("active_menu");
-window.scrollTo(0,0);
-
+    linksContainer.classList.toggle("active_menu");
 })
-
-
-
 
 
 window.addEventListener("scroll", ()=>{
@@ -36,20 +25,39 @@ window.addEventListener("scroll", ()=>{
     else{
         navHeight.classList.remove("fixed")
     }
-    return fixedNav
+  
 })
 
 
 
 links.forEach(link =>{
     link.addEventListener("click", (e)=>{
-        const fixedNav = navHeight.getBoundingClientRect().height;   
-    const slicedLink =  link.querySelector("a").getAttribute("href").slice(1);
-    const navheight = document.getElementById(slicedLink).getBoundingClientRect().height + fixedNav +600;
-    window.scrollTo(0, 500)
-        nav.classList.toggle("active_menu")
+        e.preventDefault();
+
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const nav = navHeight.getBoundingClientRect().height
+    const fixedNav = navHeight.classList.contains("fixed");
+    let position = element.offsetTop - nav;
+
+     if (!fixedNav) {
+      position = position - navHeight;
+    }
+
+    if (navHeight > 82) {
+        position = position + containerHeight;
+      }
+
+      window.scrollTo({
+        left: 0,
+        top: position,
+      });
+    linksContainer.classList.toggle("active_menu")
+});
+    
     })
-})
+
 
 
 
